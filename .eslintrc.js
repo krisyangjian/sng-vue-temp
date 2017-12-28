a3208cc70d885220cdbd824e5faf9eefa085c696
yangjian <yangjian@megvii.com>
2017-12-28 15:58:43 +0800

    eslint modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     'no-mixed-spaces-and-tabs': 'off',
     'global-require': 'off', //全局的require
     'no-debugger': process.env.NODE_ENV === 'development'? 0 : 2,
-    'no-alert': process.env.NODE_ENV === 'development'? 0 : 2,
-    'no-console': 'off' //暂时先关掉
+    'no-alert': process.env.NODE_ENV === 'development'? 0: 2,
+    'no-console': 'off' //暂时先关掉，因为prod也有可能需要输入
   }
 }
