d7cb3ca7f044ddfcb1c25e3ffca2a96f809d1f75
yangjian <yangjian@megvii.com>
2018-01-02 16:45:09 +0800

    add dir

---
+++ src/components/local/layout/header.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="header">
+    <el-button
+      style="float:right"
+      type="primary"
+      @click="logOut()">退出</el-button>
+    <div class="userinfo">
+      <h3>{{ $store.state.user.name }}</h3>
+      <h3>{{ username.name }}</h3>
+    </div>
+    <div class="header-menu">
+      <el-menu
+        :router="true"
+        class=""
+        mode="horizontal">
+        <el-menu-item index="/module1">
+          <router-link
+            class="head-menu-item"
+            to="/module1">模块1</router-link>
+        </el-menu-item>
+        <el-menu-item index="/module2">
+          <router-link
+            class="head-menu-item"
+            to="/module2">模块2</router-link>
+        </el-menu-item>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    username() {
+      return this.$store.getters.userinfo;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$router.push('../login');
+    }
+  }
+};
+</script>
+
+<style scoped="scoped">
+  .userinfo {
+    height: 100%;
+    float: left;
+    max-width: 200px;
+    overflow: hidden;
+  }
+  h3 {
+    /* display: inline-block; */
+    max-width: 200px;
+    overflow: hidden;
+  }
+	.header{
+    overflow: hidden;
+	  height: 60px;
+		background:#21539b;
+		color:#fff;
+	}
+  .header-menu {
+    height: 100%;
+    display: inline-block;
+  }
+  .header-menu .head-menu-item {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+  }
+  .header-menu .el-menu {
+    background-color: #21539b;
+    /* color: #ffffff; */
+  }
+  .header-menu .el-menu .el-menu-item {
+    padding: 0px;
+  }
+  .header-menu .router-link-active {
+    background-color: #ffffff;
+  }
+  .header-menu ul {
+    overflow: hidden;
+    list-style-type: none;
+  }
+  .header-menu ul li {
+    line-height: 60px;
+    height: 60px;
+    float: left;
+    width: 100px;
+    text-align: center;
+  }
+</style>
---
+++ src/components/local/layout/menu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="height: 100%;">
+    <!-- <el-menu
+      :router="true"
+      :unique-opened="true"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+      style="height:900px;">
+      <el-menu-item index="/module1/sub1">模块1-1</el-menu-item>
+      <el-menu-item index="/module1/sub2">模块1-2</el-menu-item>
+      <el-menu-item index="/module2/sub1">模块2-1</el-menu-item>
+      <el-menu-item index="/module2/sub2">模块2-2</el-menu-item>
+    </el-menu> -->
+    <ul class="sub-menu">
+      <router-link
+        to="/module1/sub1"
+        tag="li">模块1-1</router-link>
+      <router-link
+        to="/module1/sub2"
+        tag="li">模块1-2</router-link>
+      <router-link
+        to="/module2/sub1"
+        tag="li">模块2-1</router-link>
+      <router-link
+        to="/module2/sub2"
+        tag="li">模块2-2</router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isCollapse: false
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+<style>
+	.el-menu-vertical-demo:not(.el-menu--collapse) {
+		width: 200px;
+		min-height: 400px;
+  }
+  .sub-menu li {
+    height: 50px;
+    line-height: 50px;
+    text-align: left;
+    padding-left: 50px;
+    cursor: pointer;
+  }
+  .sub-menu li.router-link-active {
+    color:#21539b;
+    background-color: #ecf5ff;
+  }
+</style>
